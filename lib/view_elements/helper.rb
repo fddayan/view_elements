24bd0530d7ee44b102c5a672b5208be1bc9fc14b
Federico Dayan <federico.dayan@gmail.com>
2017-08-29 12:50:47 -0300

    not using helpers

--- lib/view_elements/helper.rb
+++ lib/view_elements/helper.rb
@@ -1,9 +1,9 @@
 module ViewElements::Helper
   # ViewElements.register_components_helpers(self)
 
-  def render_view_element(name, locals = {})
-    ViewElements::Locator.build(self, name, locals).render
-  end
+  # def render_view_element(name, locals = {})
+  #   ViewElements::Locator.build(self, name, locals).render
+  # end
 
   def cmp(name, locals = {})
     ViewElements::Components::Renderer.new(self.controller.view_context, name, locals).render
@@ -16,6 +16,9 @@ module ViewElements::Helper
     JAVASCRIPT
   end
 
-  alias_method :el, :render_view_element
+  # alias_method :el, :render_view_element
+
+  alias_method :render_view_element, :cmp
+  alias_method :el, :cmp
 
 end

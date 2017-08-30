ff0728c5f73de950ebee8ae98585afbc35569e41
Federico Dayan <federico.dayan@gmail.com>
2017-08-30 09:28:22 -0300

    return css to rendering manully

---
+++ lib/view_elements/Sass.rb
@@ -0,0 +1,12 @@
+module ViewElements
+  class Sass
+    def self.files
+      components_path = ViewElements.configuration.components_path
+      Dir.glob(components_path.join('**/style**'))
+      .select { |dir| File.file?(dir) }
+      .map { |dir| Pathname.new(dir).relative_path_from(components_path) }
+      # prybug
+      # "@import 'gifts/your_order/card_list';"
+    end
+  end
+end

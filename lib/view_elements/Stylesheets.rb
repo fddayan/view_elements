6099d42c45422b36ce8db17d2e42d86883847004
Federico Dayan <federico.dayan@gmail.com>
2017-08-30 11:22:18 -0300

    SAVEPOINT

--- lib/view_elements/Stylesheets.rb
+++ lib/view_elements/Stylesheets.rb
@@ -1,5 +1,5 @@
 module ViewElements
-  class Stylesheets
+  module Stylesheets
     def self.files
       components_path = ViewElements.configuration.components_path
       Dir.glob(components_path.join('**/style**'))

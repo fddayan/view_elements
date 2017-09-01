02875af7591018288128d6bc03abb33ff26fccf5
Federico Dayan <federico.dayan@gmail.com>
2017-09-01 10:24:12 -0300

    cleanup stable

--- lib/view_elements/assets.rb
+++ lib/view_elements/assets.rb
@@ -5,8 +5,13 @@ module ViewElements
       Dir.glob(components_path.join('**/style**'))
       .select { |dir| File.file?(dir) }
       .map { |dir| Pathname.new(dir).relative_path_from(components_path) }
-      # prybug
-      # "@import 'gifts/your_order/card_list';"
+    end
+
+    def self.js_files
+      components_path = ViewElements.configuration.components_path
+      Dir.glob(components_path.join('**/component**'))
+      .select { |dir| File.file?(dir) }
+      .map { |dir| Pathname.new(dir).relative_path_from(components_path) }
     end
   end
 end
--- lib/view_elements/helper.rb
+++ lib/view_elements/helper.rb
@@ -9,13 +9,6 @@ module ViewElements::Helper
     ViewElements::Components::Renderer.new(self.controller.view_context, name, locals).render
   end
 
-  def jquery_render(selector, view_name, locals = {})
-    <<-JAVASCRIPT
-    $('#{selector}')
-    .replaceWith("#{escape_javascript(el(view_name, locals))}")
-    JAVASCRIPT
-  end
-
   # alias_method :el, :render_view_element
 
   alias_method :render_view_element, :cmp

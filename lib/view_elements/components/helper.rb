5e36c3eb5d771171094d56635a470ffca6daf5f5
Federico Dayan <federico.dayan@gmail.com>
2017-09-01 13:44:29 -0300

    cleanup stable

---
+++ lib/view_elements/components/helper.rb
@@ -0,0 +1,12 @@
+module ViewElements
+  module Components
+    module Helper
+      def cmp(name, locals = {})
+        ViewElements::Components::Renderer.new(self.controller.view_context, name, locals).render
+      end
+
+      alias_method :render_view_element, :cmp
+      alias_method :el, :cmp
+    end
+  end
+end
--- lib/view_elements/railtie.rb
+++ lib/view_elements/railtie.rb
@@ -3,7 +3,7 @@ require 'view_elements/helper'
 module ViewElements
   class Railtie < ::Rails::Engine
     initializer 'view_elements.add_helpers' do
-      ActionView::Base.send :include, ViewElements::Helper
+      ActionView::Base.send :include, ViewElements::Components::Helper
     end
     initializer "view_elements.assets" do |app|
       Rails.application.config.assets.paths <<

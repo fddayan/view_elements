db8fe997a4b1bc31a47ff4dec5177882122e6dd8
Federico Dayan <federico.dayan@gmail.com>
2017-09-20 09:01:53 -0300

    using pr alias for presenter

--- lib/view_elements/component.rb
+++ lib/view_elements/component.rb
@@ -54,7 +54,7 @@ module ViewElements
         new_locals[:element] = presenter
         new_locals[:e] = presenter
         new_locals[:presenter] = presenter
-        new_locals[:p] = presenter
+        new_locals[:pr] = presenter
         temp = presenter.expose_locals
         new_locals.merge!(temp) if temp
       end
